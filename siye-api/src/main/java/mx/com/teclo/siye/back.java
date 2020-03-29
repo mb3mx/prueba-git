5e82b52d00f2088363b0e291ce55051d4887a171
fjmb <fjmb.mx@gmail.com>
2020-03-29 17:15:49 -0600

    Segunda integracion consquash

--- siye-api/src/main/java/mx/com/teclo/siye/back.java
+++ siye-api/src/main/java/mx/com/teclo/siye/back.java
@@ -1,4 +1,17 @@
 
 public class ServletInitializer   {
  //Mi clasesita
+<<<<<<< HEAD
+=======
+
+ 
+Hola commit 4
+Hola commit 4
+
+Hola commit 5
+Hola commit 5
+
+Hola commit 6
+Hola commit 6
+>>>>>>> front
 }
