e245a71d3e62120a7fcbb09606fd16d9e5d7d21d
fjmb <fjmb.mx@gmail.com>
2020-03-29 17:18:30 -0600

    Correccion

--- siye-api/src/main/java/mx/com/teclo/siye/back.java
+++ siye-api/src/main/java/mx/com/teclo/siye/back.java
@@ -1,17 +1,8 @@
 
 public class ServletInitializer   {
  //Mi clasesita
-<<<<<<< HEAD
-=======
-
  
 Hola commit 4
-Hola commit 4
-
-Hola commit 5
-Hola commit 5
-
-Hola commit 6
-Hola commit 6
->>>>>>> front
+Hola commit 5 
+Hola commit 6 
 }
