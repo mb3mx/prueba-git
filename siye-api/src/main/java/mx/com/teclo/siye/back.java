c524e0899de94a3654734357164d6d051fd9d53a
fjmb <fjmb.mx@gmail.com>
2020-03-29 19:59:52 -0600

    tercera moficiacion a mi clase java

--- siye-api/src/main/java/mx/com/teclo/siye/back.java
+++ siye-api/src/main/java/mx/com/teclo/siye/back.java
@@ -2,6 +2,7 @@
 public class ServletInitializer   {
 For other developers tracking this remote you have to ask them to pull or fetch --prune then do branch -d on your local machine to delete the local branch
 There is a lesser known fact that you can delete remote branches by pushing into them.
+Notice the . where a remote should be. also branch to branch pushing is branch1:branch2
 
 
 }
