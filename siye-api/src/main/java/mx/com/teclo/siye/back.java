ecde0a52eec6f12a9fa86c6a665eb9ed13a785d8
fjmb <fjmb.mx@gmail.com>
2020-03-29 19:59:31 -0600

    Segunda moficiacion a mi clase java

--- siye-api/src/main/java/mx/com/teclo/siye/back.java
+++ siye-api/src/main/java/mx/com/teclo/siye/back.java
@@ -1,6 +1,7 @@
 
 public class ServletInitializer   {
 For other developers tracking this remote you have to ask them to pull or fetch --prune then do branch -d on your local machine to delete the local branch
+There is a lesser known fact that you can delete remote branches by pushing into them.
 
 
 }
